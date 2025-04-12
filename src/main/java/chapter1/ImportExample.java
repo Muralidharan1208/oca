d60e11d8b74e8644c4ea4bbfb130c0ea5e7c3964
Muralidharan1208 <136390511+Muralidharan1208@users.noreply.github.com>
2025-04-12 18:36:08 +0530

    second commit

--- src/main/java/chapter1/ImportExample.java
+++ src/main/java/chapter1/ImportExample.java
@@ -1,8 +1,14 @@
 package chapter1;
 
+import java.util.Random;
+
 public class ImportExample {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Random r = new Random();
+
+        System.out.println(r.nextInt(11));
     }
     
 }
